0bd07a71b43fb7c5f8697e2c2bc9a868a49deeb2
Swaminathan Chellappa <65108616+swamisea@users.noreply.github.com>
2025-06-06 01:07:25 -0700

    part1 ex8 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,39 +8,54 @@ const Heading = ({ heading }) => {
   );
 };
 
+const Statistics = ({ total, average, positive }) => {
+  return (
+    <div>
+      all {total}
+      <br />
+      average {average}
+      <br />
+      positive {positive} %
+    </div>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0);
-  const postitive = good/total;
-  const average = ((good*1) + (bad*-1))/total
+  const positive = good / total;
+  const average = (good * 1 + bad * -1) / total;
 
   return (
     <div>
       <Heading heading="give feedback" />
-      <button onClick={
-        () => {
-          setGood(good + 1)
-          setTotal(total+1)
-        }
-    }
-    >good</button>
-      <button onClick={
-        () => {
-          setNeutral(neutral + 1)
-          setTotal(total+1)
-        }
-      }
-        >neutral</button>
-      <button onClick={
-        () => {
-          setBad(bad + 1)
-          setTotal(total+1)
-        }
-      }
-        >bad</button>
+      <button
+        onClick={() => {
+          setGood(good + 1);
+          setTotal(total + 1);
+        }}
+      >
+        good
+      </button>
+      <button
+        onClick={() => {
+          setNeutral(neutral + 1);
+          setTotal(total + 1);
+        }}
+      >
+        neutral
+      </button>
+      <button
+        onClick={() => {
+          setBad(bad + 1);
+          setTotal(total + 1);
+        }}
+      >
+        bad
+      </button>
       <Heading heading="statistics" />
       <p>
         good {good}
@@ -49,11 +64,7 @@ const App = () => {
         <br />
         bad {bad}
         <br />
-        all {total}
-        <br />
-        average {average}
-        <br />
-        positive {postitive} %
+        <Statistics total={total} average={average} positive={positive}/>
       </p>
     </div>
   );
