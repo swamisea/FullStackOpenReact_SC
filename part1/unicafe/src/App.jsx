c114e3a7d560221b19ee5b537eec70091f7206e3
Swaminathan Chellappa <65108616+swamisea@users.noreply.github.com>
2025-06-06 01:04:02 -0700

    part1 ex7 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,13 +13,34 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const postitive = good/total;
+  const average = ((good*1) + (bad*-1))/total
 
   return (
     <div>
       <Heading heading="give feedback" />
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <button onClick={
+        () => {
+          setGood(good + 1)
+          setTotal(total+1)
+        }
+    }
+    >good</button>
+      <button onClick={
+        () => {
+          setNeutral(neutral + 1)
+          setTotal(total+1)
+        }
+      }
+        >neutral</button>
+      <button onClick={
+        () => {
+          setBad(bad + 1)
+          setTotal(total+1)
+        }
+      }
+        >bad</button>
       <Heading heading="statistics" />
       <p>
         good {good}
@@ -27,6 +48,12 @@ const App = () => {
         neutral {neutral}
         <br />
         bad {bad}
+        <br />
+        all {total}
+        <br />
+        average {average}
+        <br />
+        positive {postitive} %
       </p>
     </div>
   );
