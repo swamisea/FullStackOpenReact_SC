63fa762495e18595ff932fa8d3214ee6ae36e708
Swaminathan Chellappa <65108616+swamisea@users.noreply.github.com>
2025-06-12 01:47:19 -0700

    part2 ex6 ex7 ex8 ex9 ex10 completed

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+
+
+const Filter = ({onChangeFunc, filterString}) => {
+  return(
+    <div>filter shown with <input onChange={onChangeFunc} value={filterString}/></div>
+  )
+}
+
+const Form = (props) =>{
+  return (
+    <form onSubmit = {props.onSubmitFunc}>
+        <div>name: <input onChange={props.handleAddPersonOnChange} value={props.newName}/></div>
+        <div>number: <input onChange={props.handleAddNumberOnChange} value={props.newNumber}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+const Person = ({name, number}) =>{
+  return <p>{name} {number}</p>
+}
+
+const Display = ({persons, filterString}) =>{
+  return(
+    <div>
+      {
+      persons
+      .filter(person => person.name.toLowerCase().startsWith(filterString))
+      .map((person,index) => <Person key={index} name={person.name} number={person.number}/>)
+      }
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterString, setFilterString] = useState('')
+
+  const handleAddPerson = (event) =>{
+    event.preventDefault()
+    var newPerson = {name: newName, number: newNumber}
+    if(persons.filter(person => person.name === newName).length){
+      alert(`${newName} is already added to phonebook`)
+    }
+    else if(persons.filter(person => person.number === newNumber).length){
+      alert(`${newNumber} is already added to phonebook`)
+    }
+    else if(!newName && !newNumber){
+      alert(`Name and Number are required fields`)
+    }
+    else if(!newName){
+      alert(`Name is a required field`)
+    }
+    else if(!newNumber){
+      alert(`Number is a required field`)
+    }
+    else{
+      setPersons(persons.concat(newPerson))
+    }
+  }
+
+  const handleAddPersonOnChange = (event) =>{
+    setNewName(event.target.value)
+  }
+
+  const handleAddNumberOnChange = (event) =>{
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterOnChange = (event) =>{
+    setFilterString(event.target.value.toLowerCase())
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter onChangeFunc={handleFilterOnChange} filterString={filterString}/>
+      <Form onSubmitFunc={handleAddPerson} handleAddPersonOnChange={handleAddPersonOnChange} handleAddNumberOnChange={handleAddNumberOnChange}
+      newName = {newName} newNumber={newNumber}/>
+      <h2>Numbers</h2>
+      <Display persons={persons} filterString={filterString}/>
+    </div>
+  )
+}
+
+export default App
