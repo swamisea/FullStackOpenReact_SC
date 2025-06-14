a68e966543715d8f1648fe3df9da62de7f9c1a75
Swaminathan Chellappa <65108616+swamisea@users.noreply.github.com>
2025-06-14 01:29:33 -0700

    part2 ex11 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 
 const Filter = ({onChangeFunc, filterString}) => {
@@ -36,15 +37,21 @@ const Display = ({persons, filterString}) =>{
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterString, setFilterString] = useState('')
+
+  const hook = () =>{
+    axios
+    .get("http://localhost:3001/persons")
+    .then(response =>{
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }
+  useEffect(hook,[])
+
 
   const handleAddPerson = (event) =>{
     event.preventDefault()
